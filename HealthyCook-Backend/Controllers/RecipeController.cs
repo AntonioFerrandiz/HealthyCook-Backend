c6fd77940393ad7c296d0f0ed0b1aba74debb235
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-16 12:40:59 -0500

    Recipe and User Controller

---
+++ HealthyCook-Backend/Controllers/RecipeController.cs
@@ -0,0 +1,52 @@
+﻿using HealthyCook_Backend.Domain.IServices;
+using HealthyCook_Backend.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeService _recipeService;
+
+        public RecipeController(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Recipe recipe)
+        {
+            try
+            {
+                recipe.UserID = 1;
+                recipe.Active = 1;
+                await _recipeService.CreateRecipe(recipe);
+                return Ok(new { message = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("GetListRecipes")]
+        [HttpGet]
+        public async Task<IActionResult> GetListRecipes()
+        {
+            try
+            {
+                var recipeList = await _recipeService.GetListRecipes();
+                return Ok(recipeList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ HealthyCook-Backend/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using HealthyCook_Backend.Domain.IServices;
+using HealthyCook_Backend.Domain.Models;
+using HealthyCook_Backend.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            try
+            {
+                var validateExistence = await _userService.ValidateExistence(user);
+                if (validateExistence)
+                {
+                    return BadRequest(new { message = "El usuario " + user.Username + " ya existe" });
+                }
+                user.Password = Encrypt.EncryptPassword(user.Password);
+                user.DateCreated = DateTime.Now;
+                await _userService.SaveUser(user);
+                return Ok(new { message = "Usuario registrado con exito." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
