6c857104d1e87f12b5be8fa6f321dee91b0f5275
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-16 12:43:03 -0500

    Recipe and user service

---
+++ HealthyCook-Backend/Services/RecipeService.cs
@@ -0,0 +1,31 @@
+﻿
+using HealthyCook_Backend.Domain.IRepositories;
+using HealthyCook_Backend.Domain.IServices;
+using HealthyCook_Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Services
+{
+    public class RecipeService: IRecipeService
+    {
+        private readonly IRecipeRepository _recipeRepository;
+
+        public RecipeService(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        public async Task CreateRecipe(Recipe recipe)
+        {
+            await _recipeRepository.CreateRecipe(recipe);
+        }
+
+        public async Task<List<Recipe>> GetListRecipes()
+        {
+            return await _recipeRepository.GetListRecipes();
+        }
+    }
+}
---
+++ HealthyCook-Backend/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using HealthyCook_Backend.Domain.IRepositories;
+using HealthyCook_Backend.Domain.IServices;
+using HealthyCook_Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task SaveUser(User user)
+        {
+            await _userRepository.SaveUser(user);
+        }
+
+        public async Task<bool> ValidateExistence(User user)
+        {
+            return await _userRepository.ValidateExistence(user);
+        }
+    }
+}
