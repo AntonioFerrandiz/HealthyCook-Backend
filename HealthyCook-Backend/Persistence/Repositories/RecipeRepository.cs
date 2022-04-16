a58487e93bd3484be4bafecd465fa081d11442b0
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-16 12:42:54 -0500

    Recipe and user Repository

---
+++ HealthyCook-Backend/Persistence/Repositories/RecipeRepository.cs
@@ -0,0 +1,40 @@
+﻿using HealthyCook_Backend.Domain.IRepositories;
+using HealthyCook_Backend.Domain.Models;
+using HealthyCook_Backend.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Persistence.Repositories
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        private readonly AplicationDbContext _context;
+        public RecipeRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateRecipe(Recipe recipe)
+        {
+            _context.Add(recipe);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Recipe>> GetListRecipes()
+        {
+            var recipesList = await _context.Recipes
+                .Where(x => x.Active == 1)
+                .Select(o => new Recipe
+            {
+                ID = o.ID,
+                Name = o.Name,
+                Description = o.Description,
+                User = new User { Username = o.User.Username }
+                }).ToListAsync();
+
+            return recipesList;
+        }
+    }
+}
---
+++ HealthyCook-Backend/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using HealthyCook_Backend.Domain.IRepositories;
+using HealthyCook_Backend.Domain.Models;
+using HealthyCook_Backend.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Persistence.Repositories
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly AplicationDbContext _context;
+
+        public UserRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task SaveUser(User user)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ValidateExistence(User user)
+        {
+            var validateExistence = await _context.Users
+                .AnyAsync(x => x.Username == user.Username);
+            return validateExistence;
+        }
+    }
+}
