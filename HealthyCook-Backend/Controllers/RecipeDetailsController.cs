840cd81c6a81b7903e219122a5326c8e004d67bd
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-17 20:54:48 -0500

    Update Recipe Controller

---
+++ HealthyCook-Backend/Controllers/RecipeDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using HealthyCook_Backend.Domain.IServices;
+using HealthyCook_Backend.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthyCook_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeDetailsController : ControllerBase
+    {
+        private readonly IRecipeDetailsService _recipeDetailsService;
+
+        public RecipeDetailsController(IRecipeDetailsService recipeDetailsService)
+        {
+            _recipeDetailsService = recipeDetailsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RecipeDetails recipeDetails)
+        {
+            try
+            {
+                recipeDetails.DateCreated = DateTime.Now;
+                await _recipeDetailsService.SaveRecipeDetails(recipeDetails);
+                return Ok(new { message = "ok recipe details" });
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("GetRecipeDetails/{recipeId}")]
+        [HttpGet]
+        public async Task<IActionResult> GetRecipeDetails(int recipeId)
+        {
+            try
+            {
+                var recipeDetail = await _recipeDetailsService.GetRecipeDetails(recipeId);
+                return Ok(recipeDetail);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
