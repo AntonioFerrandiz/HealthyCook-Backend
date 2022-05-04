8b9aae3b8de300767b4f5ff79d90991e6d843c96
Antonio Ferrándiz <antoniojfb01@gmail.com>
2022-05-04 18:27:27 -0500

    fixing query

--- HealthyCook-Backend/Persistence/Repositories/RecipeRepository.cs
+++ HealthyCook-Backend/Persistence/Repositories/RecipeRepository.cs
@@ -36,7 +36,7 @@ namespace HealthyCook_Backend.Persistence.Repositories
         {
             var recipesList = await _context.Recipes
                 .FromSqlRaw("select top 5  r.ID, r.Name, r.Description, r.Preparation, r.Active, r.Published, r.UserID, rd.DateCreated, rd.PreparationTime, rd.TimePeriod, rd.Servings, rd.Difficulty, rd.Calories " +
-                "from [dbo].[RecipeDetails] as rd left outer join [dbo].[Recipes] as r on rd.RecipeID = r.ID")
+                "from [dbo].[RecipeDetails] as rd left outer join [dbo].[Recipes] as r on rd.RecipeID = r.ID where r.Published = 1 and r.Active = 1 order by r.ID desc")
                 .ToListAsync();
             return recipesList;
         }
